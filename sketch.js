063115052361b04dc433c986ee09313b97379d17
devin williams <dwilliams@wlapcs.org>
2017-03-31 17:36:18 +0000

    first commit

---
+++ sketch.js
@@ -0,0 +1,32 @@
+function setup(){
+ createCanvas(800,600);
+ background(255,100,255);
+}
+
+function draw(){
+  stroke(255,255,255);
+  fill(100,90,255)
+  rect(290,100,155,250);
+  
+  stroke(0,0,0);
+  strokeWeight(4);
+  fill(90,200,155);
+  ellipse(200,200,100,100);
+  
+  line(35,20,600,400);
+  
+  fill(255,100,100)
+  ellipse(30,200,100,100);
+  
+  fill(155,30,100)
+  ellipse(60,50,100,100);
+  
+  fill(255,255,0)
+  rect(475,30,150,100)
+  
+  line(20,400,400,400)
+  
+  
+  
+
+}
